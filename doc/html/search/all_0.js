f43f052013c8ea6a2358b3b330e4e87d6fdf6b94
JoDaniel1412 <jodaniel1412@gmail.com>
2019-03-14 01:10:35 -0600

    Added the documentation for the project

---
+++ doc/html/search/all_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['clist',['CList',['../class_c_list.html',1,'']]],
+  ['cnode',['CNode',['../class_c_node.html',1,'']]],
+  ['collector',['Collector',['../class_collector.html',1,'']]]
+];
